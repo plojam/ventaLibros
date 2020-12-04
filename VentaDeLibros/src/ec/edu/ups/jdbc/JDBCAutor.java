bcbdc8fb6365889c12296be1b57fcedf073e334c
plojam <52389262+plojam@users.noreply.github.com>
2020-12-03 19:21:19 -0500

    Agregacion de controlador

---
+++ VentaDeLibros/src/ec/edu/ups/jdbc/JDBCAutor.java
@@ -0,0 +1,27 @@
+package ec.edu.ups.jdbc;
+
+import ec.edu.ups.modelo.Autor;
+
+public class JDBCAutor extends JDBCGeneric<Autor, Integer> {
+
+	public void create(Autor entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void read(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(Autor entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delete(Autor entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ VentaDeLibros/src/ec/edu/ups/jdbc/JDBCFisico.java
@@ -0,0 +1,27 @@
+package ec.edu.ups.jdbc;
+
+import ec.edu.ups.modelo.Fisico;
+
+public class JDBCFisico extends JDBCGeneric<Fisico, Integer> {
+
+	public void create(Fisico entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void read(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(Fisico entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delete(Fisico entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
